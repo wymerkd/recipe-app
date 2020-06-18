624f9b1696007dfb09acd568acf6caee992dfa13
wymerkd <wymerkd@gmail.com>
2020-06-17 20:07:41 -0700

    map through returned recipe data from API and display via props in Recipe.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useEffect, useState} from 'react';
 import './App.css';
-import Recipes from './components/Recipes';
+import Recipe from './components/Recipe';
 
 const App = () => {
 
@@ -20,6 +20,7 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data.hits);
   }
 
   return (
@@ -28,6 +29,13 @@ const App = () => {
         <input className="search-bar" type="text" />
         <button className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 }
