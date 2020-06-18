d24bd8beec5f549b8030e1028ea16720a2e5e180
wymerkd <wymerkd@gmail.com>
2020-06-17 18:09:28 -0700

    add api id and key to app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = 'a05e62b8';
+  const APP_KEY = '3844746ca92b67be7621b225f08c0f97';
+
   return (
     <div className="App">
       <h1>Hello React</h1>
