83dc686d220ab0473545a75d63006f5809519d05
wymerkd <wymerkd@gmail.com>
2020-06-17 18:18:14 -0700

    store get request as a constant

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ const App = () => {
   const APP_ID = 'a05e62b8';
   const APP_KEY = '3844746ca92b67be7621b225f08c0f97';
 
+  const recipeReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+
   return (
     <div className="App">
       <h1>Hello React</h1>
