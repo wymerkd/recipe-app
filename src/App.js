3f755dd67f0e5a55b1abe45d2bae38ef4bda9cd6
wymerkd <wymerkd@gmail.com>
2020-06-17 21:54:47 -0700

    add search function, store search result state

--- src/App.js
+++ src/App.js
@@ -9,16 +9,17 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
 
 
   // useEffect will only run once when page is rendered
   useEffect( () => {
     getRecipes();
-  }, [search]);
+  }, [query]);
 
   // API request, waits for the response and converts to json
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
@@ -29,9 +30,15 @@ const App = () => {
     console.log(search);
   }
 
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
   return (
     <div className="App">
-      <form className="search-form">
+      <form className="search-form" onSubmit={getSearch}>
         <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">Search</button>
       </form>
