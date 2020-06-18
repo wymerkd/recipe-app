36e914bcc5acd45cc828a6438007c4f609dec725
wymerkd <wymerkd@gmail.com>
2020-06-17 19:29:46 -0700

    create async function to fetch recipe data from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
@@ -6,8 +6,17 @@ const App = () => {
   const APP_ID = 'a05e62b8';
   const APP_KEY = '3844746ca92b67be7621b225f08c0f97';
 
-  const recipeReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  // useEffect will only run once when page is rendered
+  useEffect( () => {
+    getRecipes();
+  }, []);
 
+  // API request, waits for the response and converts to json
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data.hits);
+  }
 
   return (
     <div className="App">
