1a41a820a8e8621c54fab11229c4b8318d3a5150
wymerkd <wymerkd@gmail.com>
2020-06-17 21:46:23 -0700

    add onChange event to search input

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ const App = () => {
   const APP_KEY = '3844746ca92b67be7621b225f08c0f97';
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
 
 
   // useEffect will only run once when page is rendered
   useEffect( () => {
     getRecipes();
-  }, []);
+  }, [search]);
 
   // API request, waits for the response and converts to json
   const getRecipes = async () => {
@@ -23,14 +24,20 @@ const App = () => {
     console.log(data.hits);
   }
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
   return (
     <div className="App">
       <form className="search-form">
-        <input className="search-bar" type="text" />
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">Search</button>
       </form>
       {recipes.map(recipe => (
         <Recipe
+          key={recipe.recipe.label}
           title={recipe.recipe.label}
           calories={recipe.recipe.calories}
           image={recipe.recipe.image}
